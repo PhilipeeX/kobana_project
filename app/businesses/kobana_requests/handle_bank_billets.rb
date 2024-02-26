2bb874462990764ca3287fbb8f33a713073492bc
Philipe <dev.philipe@outlook.com>
2024-02-26 20:09:32 -0300

    feat(handleBankBillets): Creates form, private method and actions for bank_billet creation

--- app/businesses/kobana_requests/handle_bank_billets.rb
+++ app/businesses/kobana_requests/handle_bank_billets.rb
@@ -1,12 +1,19 @@
 module KobanaRequests
   # This \class Handles requests for bank billets which comes from kobana api
   class HandleBankBillets < BusinessApplication
-    def initialize(action)
+    def initialize(action, params = {})
       @action = action
+      @params = params
     end
 
     def call
-      @action == :index ? index_billets : "#{I18n.t('businesses.kobana_requests.unknown_action')}: #{@action}"
+      if @action == :index
+        index_billets
+      elsif @action == :create
+        create_bank_billet
+      else
+        "#{I18n.t('businesses.kobana_requests.unknown_action')}: #{@action}"
+      end
     end
 
     private
@@ -23,6 +30,21 @@ module KobanaRequests
 
       response = http.request(request)
       JSON.parse(response.body)
+    end
+
+    def create_bank_billet
+      url = URI('https://api-sandbox.kobana.com.br/v1/bank_billets')
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Post.new(url)
+      request['accept'] = 'application/json'
+      request['content-type'] = 'application/json'
+      request['authorization'] = "Bearer #{Rails.application.credentials.dig(:api, :boletosimples, :access_token)}"
+      request.body = @params.to_json
+
+      http.request(request)
     end
   end
 end
--- app/controllers/bank_billets_controller.rb
+++ app/controllers/bank_billets_controller.rb
@@ -2,4 +2,27 @@ class BankBilletsController < ApplicationController
   def index
     @billets = KobanaRequests::HandleBankBillets.call(:index)
   end
+
+  def new
+  end
+
+  def create
+    response = KobanaRequests::HandleBankBillets.call(:create, billet_params)
+    if JSON.parse(response.body)['status'] == 'generating'
+      redirect_to action: 'index'
+    elsif JSON.parse(response.body).include? 'errors'
+      errors = JSON.parse(response.body)['errors']
+      error_messages = errors.map do |field, messages|
+        "#{field}: #{messages.join(', ')}"
+      end
+      flash[:notice] = "Esses erros impediram a criação do boleto: #{error_messages.join('. ')}"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def billet_params
+    params.permit(:authenticity_token, :amount, :expire_at, :customer_person_name, :customer_cnpj_cpf, :customer_state, :customer_city_name, :customer_zipcode, :customer_address, :customer_neighborhood)
+  end
 end
